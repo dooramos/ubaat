8b3aa182030a1f578cfe970cd0f02d038ee2213c
Eduardo Ramos <dooramos@gmail.com>
2026-05-04 00:45:51 -0300

    atualizado-04-05-2026

--- main.js
+++ main.js
@@ -1,30 +1,29 @@
-
-const nav = document.querySelector('#header nav')
-const toggle = document.querySelectorAll('nav .toggle')
+const nav = document.querySelector("#header nav");
+const toggle = document.querySelectorAll("nav .toggle");
 for (const element of toggle) {
-  element.addEventListener('click', function () {
-    nav.classList.toggle('show')
-  })
+  element.addEventListener("click", function () {
+    nav.classList.toggle("show");
+  });
 }
 
-const links = document.querySelectorAll('nav ul li a')
+const links = document.querySelectorAll("nav ul li a");
 for (const link of links) {
-  link.addEventListener('click', function () {
-    nav.classList.remove('show')
-  })
+  link.addEventListener("click", function () {
+    nav.classList.remove("show");
+  });
 }
 
-const header = document.querySelector('#header')
-const navHeight = header.offsetHeight
+const header = document.querySelector("#header");
+const navHeight = header.offsetHeight;
 
 function changeHeaderWhenScroll() {
   if (window.scrollY >= navHeight) {
-    header.classList.add('scroll')
+    header.classList.add("scroll");
   } else {
-    header.classList.remove('scroll')
+    header.classList.remove("scroll");
   }
 }
- 
+
 const swpArteterapia = new Swiper(".swpArteterapia", {
   slidesPerView: 3,
   spaceBetween: 30,
@@ -39,78 +38,74 @@ const swpArteterapia = new Swiper(".swpArteterapia", {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
   },
-  breakpoints: 
-  {
-      320: {
-        slidesPerView: 1,
-        spaceBetween: 20,
-      },
-      480: {
-        slidesPerView: 1,
-        spaceBetween: 20,
-      },
-      640: {
-        slidesPerView: 1,
-        spaceBetween: 20,
-      },
-      768: {
-        slidesPerView: 3,
-        spaceBetween: 40,
-      },
-      1024: {
-        slidesPerView: 3,
-        spaceBetween: 50,
-      }
-}
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 20,
+    },
+    480: {
+      slidesPerView: 1,
+      spaceBetween: 20,
+    },
+    640: {
+      slidesPerView: 1,
+      spaceBetween: 20,
+    },
+    768: {
+      slidesPerView: 3,
+      spaceBetween: 40,
+    },
+    1024: {
+      slidesPerView: 3,
+      spaceBetween: 50,
+    },
+  },
 });
-const  swpArteterapiaTipos = new Swiper(".swpArteterapiaTipos", {
+const swpArteterapiaTipos = new Swiper(".swpArteterapiaTipos", {
   navigation: {
     nextEl: ".swiper-button-next-art2",
     prevEl: ".swiper-button-prev-art2",
-  } 
-});
-const  swpAssociacoes = new Swiper(".swpAssociacoes", {
-  
-  pagination: {
-    el: '.swiper-pagination'
   },
-  
+});
+const swpAssociacoes = new Swiper(".swpAssociacoes", {
+  pagination: {
+    el: ".swiper-pagination",
+  },
+
   autoplay: {
     delay: 5000,
   },
   breakpoints: {
     320: {
       slidesPerView: 1,
-      setWrapperSize: true
+      setWrapperSize: true,
     },
     480: {
       slidesPerView: 1,
-      setWrapperSize: true
+      setWrapperSize: true,
     },
     640: {
       slidesPerView: 3,
-      setWrapperSize: true
+      setWrapperSize: true,
     },
     767: {
       slidesPerView: 3,
-      setWrapperSize: true
-    }
-  }
+      setWrapperSize: true,
+    },
+  },
 });
 
 const scrollReveal = ScrollReveal({
-  origin: 'top',
-  distance: '30px',
+  origin: "top",
+  distance: "30px",
   duration: 700,
-  reset: true
+  reset: true,
 });
 
-
-
 scrollReveal.reveal(
- `#warning a,
+  `#warning a,
   #home a,
-  #about .hightitle,
+  #sobre .hightitle,
   #cursos .tablinks, 
   #arteterapia h2,  
   #arteterapia2 .swipper,
@@ -127,48 +122,52 @@ scrollReveal.reveal(
   { interval: 300 }
 );
 
-const backToTopButton = document.querySelector('.back-to-top');
+const backToTopButton = document.querySelector(".back-to-top");
 
 function backToTop() {
   if (window.scrollY >= 560) {
-    backToTopButton.classList.add('show')
+    backToTopButton.classList.add("show");
   } else {
-    backToTopButton.classList.remove('show')
+    backToTopButton.classList.remove("show");
   }
 }
 
 function activateMenuAtCurrentSection() {
-  const sections = document.querySelectorAll('main section.section[id]');  
-  const checkpoint = window.pageYOffset + (window.innerHeight / 8) * 4
+  const sections = document.querySelectorAll("main section.section[id]");
+  const checkpoint = window.pageYOffset + (window.innerHeight / 8) * 4;
 
   for (const section of sections) {
-   
-    const sectionTop = section.offsetTop
-    const sectionHeight = section.offsetHeight
-    const sectionId = section.getAttribute('id')
+    const sectionTop = section.offsetTop;
+    const sectionHeight = section.offsetHeight;
+    const sectionId = section.getAttribute("id");
 
-    const checkpointStart = checkpoint >= sectionTop
-    const checkpointEnd = checkpoint <= sectionTop + sectionHeight
+    const checkpointStart = checkpoint >= sectionTop;
+    const checkpointEnd = checkpoint <= sectionTop + sectionHeight;
 
-    if (checkpointStart && checkpointEnd) 
-    {
-      if(document.querySelector('nav ul li a[href*=' + sectionId + ']')!=undefined        
-      && document.querySelector('nav ul li a[href*=' + sectionId + ']')!=null)
-      {
-        document.querySelector('nav ul li a[href*=' + sectionId + ']').classList.add('active')
+    if (checkpointStart && checkpointEnd) {
+      if (
+        document.querySelector("nav ul li a[href*=" + sectionId + "]") !=
+          undefined &&
+        document.querySelector("nav ul li a[href*=" + sectionId + "]") != null
+      ) {
+        document
+          .querySelector("nav ul li a[href*=" + sectionId + "]")
+          .classList.add("active");
       }
-    } 
-    else
-     {             
-        if(document.querySelector('nav ul li a[href*=' + sectionId + ']')!=undefined        
-        && document.querySelector('nav ul li a[href*=' + sectionId + ']')!=null)
-        {
-          document.querySelector('nav ul li a[href*=' + sectionId + ']').classList.remove('active')     
-        }
+    } else {
+      if (
+        document.querySelector("nav ul li a[href*=" + sectionId + "]") !=
+          undefined &&
+        document.querySelector("nav ul li a[href*=" + sectionId + "]") != null
+      ) {
+        document
+          .querySelector("nav ul li a[href*=" + sectionId + "]")
+          .classList.remove("active");
+      }
     }
   }
 }
-function closeTab(){  
+function closeTab() {
   var i, tabcontent, tablinks;
   tabcontent = document.getElementsByClassName("tabcontent");
   for (i = 0; i < tabcontent.length; i++) {
@@ -178,10 +177,9 @@ function closeTab(){
   for (i = 0; i < tablinks.length; i++) {
     tablinks[i].className = tablinks[i].className.replace(" active", "");
   }
-  document.getElementById('closeTab').style.display = "none";
+  document.getElementById("closeTab").style.display = "none";
 }
 function openTab(evt, tabName) {
-  
   var i, tabcontent, tablinks;
   tabcontent = document.getElementsByClassName("tabcontent");
   for (i = 0; i < tabcontent.length; i++) {
@@ -192,139 +190,191 @@ function openTab(evt, tabName) {
     tablinks[i].className = tablinks[i].className.replace(" active", "");
   }
   document.getElementById(tabName).style.display = "block";
-  document.getElementById('closeTab').style.display = "block";
+  document.getElementById("closeTab").style.display = "block";
   evt.currentTarget.className += " active";
-
 }
 
 function getItemByRegion(regiao, index) {
-  let _html ='<h3>' + regiao.regiao + '</h3>';
-  let _container= "";
-  switch(regiao.regiao){
-    case 'Região Centro-Oeste':
-      _container ="centrooeste";
+  let _html = "<h3>" + regiao.regiao + "</h3>";
+  let _container = "";
+  switch (regiao.regiao) {
+    case "Região Centro-Oeste":
+      _container = "centrooeste";
       break;
-      case 'Região Nordeste':
-        _container ="nordeste";
+    case "Região Nordeste":
+      _container = "nordeste";
       break;
-      case 'Região Norte':
-        _container ="norte";
+    case "Região Norte":
+      _container = "norte";
       break;
-      case 'Região Sul':
-        _container ="sul";
+    case "Região Sul":
+      _container = "sul";
       break;
-      case 'Região Sudeste':
-        _container ="sudeste";
+    case "Região Sudeste":
+      _container = "sudeste";
       break;
   }
-  let destination =document.getElementById(_container);
-  regiao.estados.forEach(function(estado, index) {
-    
-    _html+='<h4 class="city '+_container+'">' + estado.nome + '</h4>';
-    _html+='<ul>';
-    estado.lista.forEach(function(curso,index){
-      _html+='<li><span class="campo">Nome: </span><span class="valor">'+curso.nome+'</span><li>';
-      _html+='<li><span class="campo">Instituição: </span><span class="valor">'+curso.instituicao+'</span><li>';
-      _html+='<li><span class="campo">Endereço: </span><span class="valor">'+curso.endereco+'</span><li>';
-      _html+='<li><span class="campo">Contato: </span><span class="valor">'+curso.telefone+'</span><li>';
-      _html+='<li><span class="campo">Site: </span><span class="valor">'+curso.site+'</span><li>';
-      _html+='<li><span class="campo">Email: </span><span class="valor">'+curso.email+'</span><li>';
-      _html+='<li><span class="campo">Coordenação: </span><span class="valor">'+curso.coordenacao+'</span><li>';
-      _html+='<li><span class="breakline"></span><li>';
-      
-    })
-    _html+='</ul>';
-    
-    destination.innerHTML=_html;
+  let destination = document.getElementById(_container);
+  regiao.estados.forEach(function (estado, index) {
+    _html += '<h4 class="city ' + _container + '">' + estado.nome + "</h4>";
+    _html += "<ul>";
+    estado.lista.forEach(function (curso, index) {
+      _html +=
+        '<li><span class="campo">Nome: </span><span class="valor">' +
+        curso.nome +
+        "</span><li>";
+      _html +=
+        '<li><span class="campo">Instituição: </span><span class="valor">' +
+        curso.instituicao +
+        "</span><li>";
+      _html +=
+        '<li><span class="campo">Endereço: </span><span class="valor">' +
+        curso.endereco +
+        "</span><li>";
+      _html +=
+        '<li><span class="campo">Contato: </span><span class="valor">' +
+        curso.telefone +
+        "</span><li>";
+      _html +=
+        '<li><span class="campo">Site: </span><span class="valor">' +
+        curso.site +
+        "</span><li>";
+      _html +=
+        '<li><span class="campo">Email: </span><span class="valor">' +
+        curso.email +
+        "</span><li>";
+      _html +=
+        '<li><span class="campo">Coordenação: </span><span class="valor">' +
+        curso.coordenacao +
+        "</span><li>";
+      _html += '<li><span class="breakline"></span><li>';
+    });
+    _html += "</ul>";
+
+    destination.innerHTML = _html;
   });
 }
 
-function getCursos(){
-  
-  fetch('./assets/cursosubaat.json')
-  .then((resp) => resp.json())
-  .then(function(data) {
-    var regioes = data.cursos;
-    regioes.forEach(getItemByRegion);
-        })
-  .catch(function(error) {
-    console.log(error);
-      });
+function getCursos() {
+  fetch("./assets/cursosubaat.json")
+    .then((resp) => resp.json())
+    .then(function (data) {
+      var regioes = data.cursos;
+      regioes.forEach(getItemByRegion);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
 }
 
-function modalDiretoria(modalToShow,openButton,closeButton){
-var modal = document.getElementById(modalToShow);
-var openB = document.getElementById(openButton);
-var closeB = document.getElementById(closeButton);
-  openB.onclick = function() {  modal.style.display = "block";   return false;}
-  if(closeB!=null){ closeB.onclick = function() {  modal.style.display = "none";   return false;}}
-  window.onclick = function(event) {
+function modalDiretoria(modalToShow, openButton, closeButton) {
+  var modal = document.getElementById(modalToShow);
+  var openB = document.getElementById(openButton);
+  var closeB = document.getElementById(closeButton);
+  openB.onclick = function () {
+    modal.style.display = "block";
+    return false;
+  };
+  if (closeB != null) {
+    closeB.onclick = function () {
+      modal.style.display = "none";
+      return false;
+    };
+  }
+  window.onclick = function (event) {
     if (event.target == modal) {
       modal.style.display = "none";
     }
-  };  
+  };
 }
 
-function modalVideo(modalToShow,openButton,closeButton){
+function modalVideo(modalToShow, openButton, closeButton) {
   var modalVi = document.getElementById(modalToShow);
   var openVi = document.getElementById(openButton);
   var closeVi = document.getElementById(closeButton);
-  
-    openVi.onclick = function() {  modalVi.style.display = "block";  return false;}
-    if(closeVi!=null){ closeVi.onclick = function() {  modalVi.style.display = "none"; return false;}}
-    window.onclick = function(event) {
-      if (event.target == modalVi) {
-        modalVi.style.display = "none";        
-      }
-    };  
+
+  openVi.onclick = function () {
+    modalVi.style.display = "block";
+    return false;
+  };
+  if (closeVi != null) {
+    closeVi.onclick = function () {
+      modalVi.style.display = "none";
+      return false;
+    };
+  }
+  window.onclick = function (event) {
+    if (event.target == modalVi) {
+      modalVi.style.display = "none";
+    }
+  };
 }
 
-function modalAviso(modalToShow,closeButton){
-    var modal = document.getElementById(modalToShow);
-    var closeB = document.getElementById(closeButton);
-    
-      if(closeB!=null){ closeB.onclick = function() {  modal.style.display = "none";   return false;}}
-      window.onclick = function(event) {
-        if (event.target == modal) {
-          modal.style.display = "none";
-        }
-      };  
-      modal.style.display = "block";  
+function modalAviso(modalToShow, closeButton) {
+  var modal = document.getElementById(modalToShow);
+  var closeB = document.getElementById(closeButton);
 
+  if (closeB != null) {
+    closeB.onclick = function () {
+      modal.style.display = "none";
+      return false;
+    };
+  }
+  window.onclick = function (event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  };
+  modal.style.display = "block";
 }
 
-function modalAssociacoes(modalToShow,openButton,closeButton){
+function modalAssociacoes(modalToShow, openButton, closeButton) {
   var modalVi = document.getElementById(modalToShow);
   var openVi = document.getElementById(openButton);
   var closeVi = document.getElementById(closeButton);
-  
-    openVi.onclick = function() {  modalVi.style.display = "block";  return false;}
-    if(closeVi!=null){ closeVi.onclick = function() {  modalVi.style.display = "none"; return false;}}
-    window.onclick = function(event) {
-      if (event.target == modalVi) {
-        modalVi.style.display = "none";        
-      }
-    };  
-}
-function contato(){
-  const nome=document.getElementById('nome');
-  const assunto=document.getElementById('assunto');
-  const mensagem=document.getElementById('mensagem');
-  const email=document.getElementById('email');
 
-if(nome.value!==''&&nome.value!==undefined&&nome.value!==null
-  && mensagem.value!==''&&mensagem.value!==undefined&&mensagem.value!==null
-  && email.value!==''&&email.value!==undefined&&email.value!==null
-  && assunto.value!==''&&assunto.value!==undefined&&assunto.value!==null
-  )
-  {   
-   
-    //fetch("https://localhost:44327/api/v1/Contato", {
-      fetch("https://api.ubaat.com.br/api/v1/Contato", {
-      method: 'POST',
+  openVi.onclick = function () {
+    modalVi.style.display = "block";
+    return false;
+  };
+  if (closeVi != null) {
+    closeVi.onclick = function () {
+      modalVi.style.display = "none";
+      return false;
+    };
+  }
+  window.onclick = function (event) {
+    if (event.target == modalVi) {
+      modalVi.style.display = "none";
+    }
+  };
+}
+function contato() {
+  const nome = document.getElementById("nome");
+  const assunto = document.getElementById("assunto");
+  const mensagem = document.getElementById("mensagem");
+  const email = document.getElementById("email");
+
+  if (
+    nome.value !== "" &&
+    nome.value !== undefined &&
+    nome.value !== null &&
+    mensagem.value !== "" &&
+    mensagem.value !== undefined &&
+    mensagem.value !== null &&
+    email.value !== "" &&
+    email.value !== undefined &&
+    email.value !== null &&
+    assunto.value !== "" &&
+    assunto.value !== undefined &&
+    assunto.value !== null
+  ) {
+  fetch("https://localhost:44327/api/v1/Contato", {
+      //fetch("https://api.ubaat.com.br/api/v1/Contato", {
+      method: "POST",
       headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json'
+        Accept: "application/json",
+        "Content-Type": "application/json",
       },
       body: `{
         "contatoDe": "${email.value}",
@@ -332,61 +382,53 @@ if(nome.value!==''&&nome.value!==undefined&&nome.value!==null
         "nome": "${nome.value}",
         "mensagem": "${mensagem.value}"  
       }`,
-    }).then((resp) => resp.json())
-    .then(function(data) {
-  
-      if(data.status === 201||data.status ===200) {
-        Swal.fire({
-          title: 'Sucesso!',
-          text: 'Mensagem Enviada',
-          icon: 'success',
-          confirmButtonText: 'Fechar'
-        });
-      }
-      else
-      {
-        Swal.fire({
-          title: 'Atenção!',
-          text: 'Ocorreu um erro no envio da mensagem :(',
-          icon: 'error',
-          confirmButtonText: 'Fechar'
-        });
-      }
-      resetFormContato();
-    
-
-    });
+    })
+      .then((resp) => resp.json())
+      .then(function (data) {
+        if (data.status === 201 || data.status === 200) {
+          Swal.fire({
+            title: "Sucesso!",
+            text: "Mensagem Enviada",
+            icon: "success",
+            confirmButtonText: "Fechar",
+          });
+        } else {
+          Swal.fire({
+            title: "Atenção!",
+            text: "Ocorreu um erro no envio da mensagem :(",
+            icon: "error",
+            confirmButtonText: "Fechar",
+          });
+        }
+        resetFormContato();
+      });
     return false;
-  } else
-  {
+  } else {
     Swal.fire({
-      title: 'Atenção!',
-      text: 'Todos os campos são obrigatórios!',
-      icon: 'warning',
-      confirmButtonText: 'Fechar'
+      title: "Atenção!",
+      text: "Todos os campos são obrigatórios!",
+      icon: "warning",
+      confirmButtonText: "Fechar",
     });
   }
 }
 
-function resetFormContato()
-{  
+function resetFormContato() {
   document.forms[0].reset();
 }
 
-
-window.addEventListener('scroll', function () {
-  changeHeaderWhenScroll()
-  backToTop()
-  activateMenuAtCurrentSection() 
-}); 
+window.addEventListener("scroll", function () {
+  changeHeaderWhenScroll();
+  backToTop();
+  activateMenuAtCurrentSection();
+});
 
 swpArteterapia.init();
 swpArteterapiaTipos.init();
 swpAssociacoes.init();
 
-modalVideo('videoModal','home','videoModalClose');
-modalAviso('homeModal','homeModalClose');
-modalDiretoria('diretoriaModal','diretoriaModalOpen','diretoriaModalClose');
-modalAssociacoes('assocModal','swpAssociacoes','assocModalClose');
+modalVideo("videoModal", "home", "videoModalClose");
+//modalAviso("homeModal", "homeModalClose");
+modalDiretoria("diretoriaModal", "diretoriaModalOpen", "diretoriaModalClose");
+modalAssociacoes("assocModal", "swpAssociacoes", "assocModalClose");
 getCursos();
-
